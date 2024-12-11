e349ff8b319509db114c3b4a189cd1ab8cc9e4f3
Vincent FERRIES <vincent.ferries@gmail.com>
2024-12-11 16:29:20 +0100

    Add new templates

---
+++ src/DayTemplate.kt
@@ -0,0 +1,15 @@
+object DayTemplate: Day() {
+    override fun p1(input: List<String>): String {
+        return ""
+    }
+
+    override fun p2(input: List<String>): String {
+        return ""
+    }
+}
+
+fun main() {
+    val input = readInput("DayTemplate")
+    DayTemplate.part1(input).println()
+    DayTemplate.part2(input).println()
+}
